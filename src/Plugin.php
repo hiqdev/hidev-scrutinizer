28ca114aa50fe79c906890a14a7007283e54bda1
Andrii Vasyliev <sol@hiqdev.com>
2015-12-23 13:20:36 +0000

    + `markdownBadges`

--- src/Plugin.php
+++ src/Plugin.php
@@ -17,6 +17,13 @@ class Plugin extends \hiqdev\pluginmanager\Plugin
         'goals' => [
             'scrutinizer'       => 'hidev\scrutinizer\goals\ScrutinizerGoal',
             '.scrutinizer.yml'  => 'hidev\scrutinizer\goals\ScrutinizerYmlGoal',
+            'readme'      => [
+                'markdownBadges' => [
+                    'scrutinizer.coverage' => '[![Code Coverage](https://scrutinizer-ci.com/g/{{ config.github.name }}/badges/coverage.png?b=master)](https://scrutinizer-ci.com/g/{{ config.github.name }}/?branch=master)',
+                    'scrutinizer.quality'  => '[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/{{ config.github.name }}/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/{{ config.github.name }}/?branch=master)',
+                    'scrutinizer.build'    => '[![Build Status](https://scrutinizer-ci.com/g/{{ config.github.name }}/badges/build.png?b=master)](https://scrutinizer-ci.com/g/{{ config.github.name }}/build-status/master)',
+                ],
+            ],
         ],
     ];
 }
