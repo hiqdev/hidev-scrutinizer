d6ead256fe163e9baac4a60df8692fcde83c56da
Andrii Vasyliev <sol@hiqdev.com>
2016-01-14 08:37:38 +0000

    - PHP external coverage defaults from scrutinizer YAML goal

--- src/controllers/ScrutinizerYamlController.php
+++ src/controllers/ScrutinizerYamlController.php
@@ -13,27 +13,8 @@ namespace hidev\scrutinizer\controllers;
 
 /**
  * Goal for .scrutinizer.yml config file.
- * For the moment PHP projects only.
  */
 class ScrutinizerYamlController extends \hidev\controllers\FileController
 {
     protected $_file = '.scrutinizer.yml';
-
-    /**
-     * Adds defaults.
-     */
-    public function actionLoad()
-    {
-        $this->setItems([
-            'tools' => [
-                'php_code_coverage' => [
-                    'enabled' => true,
-                ],
-                'external_code_coverage' => [
-                    'timeout' => 600,
-                ],
-            ],
-        ]);
-        parent::actionLoad();
-    }
 }
