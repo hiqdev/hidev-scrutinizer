b73de0d502a9fdb01b3a054362c8198def18c874
Andrii Vasyliev <sol@hiqdev.com>
2017-05-03 15:44:58 +0200

    csfixed

--- src/components/ScrutinizerYaml.php
+++ src/components/ScrutinizerYaml.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Scrutinizer plugin for HiDev
+ *
+ * @link      https://github.com/hiqdev/hidev-scrutinizer
+ * @package   hidev-scrutinizer
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
+ */
 
 namespace hidev\scrutinizer\components;
 
--- src/console/ScrutinizerController.php
+++ src/console/ScrutinizerController.php
@@ -1,12 +1,11 @@
 <?php
-
-/*
+/**
  * Scrutinizer plugin for HiDev
  *
  * @link      https://github.com/hiqdev/hidev-scrutinizer
  * @package   hidev-scrutinizer
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
  */
 
 namespace hidev\scrutinizer\console;
